1b868f23f6edbf68590d591f7e51674713da6bab
AgentSMDJ <sandermae036@gmail.com>
2026-09-04 08:52:21 +0200

    set all the sections in variables and .loged them (services, advantages, sites, facilities, hero)

--- js/index.js
+++ js/index.js
@@ -1,4 +1,117 @@
 // slå dig løs her... 
 
+
 //eksempel på at udskrive alle overskrifter i services i konsollen:
 services.forEach(service => console.log(service.headline))
+
+// vraiables ---------------------------------------------------------------
+    // find section hero and display in console for better overview
+    const heroSection = document.querySelector(".hero");
+    console.log(heroSection)
+
+        // make <img> with src and alt
+
+
+        // make <h1>
+
+
+        // make dummy txt in a <p> element
+
+
+        // make a button with
+
+            // insert the icon <img>
+
+            // insert <p> with thext explore 
+
+
+    // find section services an display in console ---------------------------------
+    const servicesSection = document.querySelector(".services");
+    console.log(servicesSection);
+
+        // make a for each that does this with every object in the array
+
+
+
+            // insert img
+
+
+            // insert h3 with txt
+
+
+            //  insert paragraph with dummy txt
+
+
+            // insert <a> with txt
+
+
+
+    // find fascilities and display in console ------------------------------------
+    const facilitiesSection = document.querySelector(".facilities");
+    console.log(facilitiesSection);
+        
+    
+        // display h2
+        
+        
+        // make an array for each object in the array
+
+            // insert img
+
+
+            // insert h3 with txt
+
+
+            // insert paragraph with txt
+
+        
+    // find sites and display in console ----------------------------------------------
+    const sitesSection = document.querySelector(".sites");
+    console.log(sitesSection);
+
+        // insert h2 with txt
+
+
+        // insert paragraph with txt
+
+
+        // insert button
+
+            // insert icon
+
+            // insert paragraph with txt
+
+
+        // insert section and display with log for better view
+
+            // make foreach object in the array = places
+
+                // insert img
+
+                // insert paragraph with name as txt
+
+                // paragraph with city name
+
+    // find advantages and display in console ------------------------------------------
+    const advantagesSection = document.querySelector(".advantages");
+    console.log(advantagesSection);
+    
+        // make a h2 with txt Our Advantages 
+
+        // make foreach object in the array 
+
+            // insert icon (img) 
+
+            // insert h3 with headline
+
+            // insert paragraph with text 
+
+
+
+
+
+
+
+
+
+// functions ----------------------------------------------------------
